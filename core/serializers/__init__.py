f4592d88b8c77dae8002a502c10c159377599cc4
Camila Ferreira de Almeida <mila.e.ferreira28@gmail.com>
2024-12-02 09:36:06 -0300

    Criação de um endpoint para criar novas compras.

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -4,4 +4,4 @@ from .editora import EditoraSerializer
 from .autor import AutorSerializer
 # from .livro import LivroSerializer, LivroListRetrieveSerializer, LivroListSerializer
 from .livro import LivroSerializer, LivroDetailSerializer, LivroListSerializer, LivroRetrieveSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer, CompraCreateUpdateSerializer, ItensCompraSerializer
